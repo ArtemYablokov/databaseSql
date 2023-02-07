2856b3de9fa593d7e490d7b3b69454211b12cd5a
Artem Yablokov <a9777150191@gmail.com>
2023-02-07 00:45:53 -0600

    3 refactoring

--- src/main/resources/constraint.sql
+++ src/main/resources/constraint.sql
@@ -2,14 +2,11 @@ ALTER TABLE student
     ADD
         CONSTRAINT proper_name
             CHECK (student.name !~ '[@|#|$]');
--- почему то наложил ограничение только на #
 
-select * from student where name like '%@|#|$%';
-select * from student where name like '@|#|$';
-
--- LIKE doesn't use REG-EX
-
-select * from student where name SIMILAR TO '@|#|$';
-select * from student where name SIMILAR TO '%@|#|$%';
-select count(*) from student where name NOT SIMILAR TO '@|#|$';
+-- select * from student where name like '%@|#|$%';
+-- select * from student where name like '@|#|$';
+--
+-- select * from student where name SIMILAR TO '@|#|$';
+-- select * from student where name SIMILAR TO '%@|#|$%';
+-- select count(*) from student where name NOT SIMILAR TO '@|#|$';
 
